27d6e66743aade52ee56aa7a67997a7bf65e232d
Siddharth Krishan <sidd_krishan@outlook.com>
2018-08-20 16:23:57 -0700

    minor change

--- CNN.py
+++ CNN.py
@@ -18,34 +18,32 @@ classifier = Sequential()
 classifier.add(Convolution2D(64, (5, 5), input_shape=(128, 128, 3), activation='relu'))
 classifier.add(Convolution2D(64, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(4, 4)))
-#classifier.add(Dense(units=32, activation='relu'))
-#classifier.add(Dropout(rate=0.1))
+# classifier.add(Dense(units=32, activation='relu'))
+# classifier.add(Dropout(rate=0.1))
 
 classifier.add(Convolution2D(64, (5, 5), activation='relu'))
 classifier.add(Convolution2D(64, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-
 classifier.add(Convolution2D(64, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-
 classifier.add(Flatten())
 
-classifier.add(Dense(units=256, activation='relu'))
+classifier.add(Dense(units=128, activation='relu'))
 classifier.add(Dropout(rate=0.2))
 
 classifier.add(Dense(units=128, activation='relu'))
 classifier.add(Dropout(rate=0.2))
 
 classifier.add(Dense(units=64, activation='relu'))
-#classifier.add(Dropout(rate=0.2))
+classifier.add(Dropout(rate=0.1))
 
 classifier.add(Dense(units=64, activation='relu'))
-#classifier.add(Dropout(rate=0.1))
-
-classifier.add(Dense(units=32, activation ='relu'))
 classifier.add(Dropout(rate=0.1))
+
+classifier.add(Dense(units=32, activation='relu'))
+#classifier.add(Dropout(rate=0.1))
 
 classifier.add(Dense(units=1, activation='sigmoid'))
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -79,9 +77,8 @@ r = classifier.fit_generator(
     epochs=100,
     validation_data=test_set,
     validation_steps=(2000 / 32))
-print("Returned: ",r)
+print("Returned: ", r)
 print(r.history.keys())
-
 
 plt.plot(r.history['loss'], label='loss')
 plt.plot(r.history['val_loss'], label='val_loss')
@@ -97,11 +94,11 @@ end_time = dt.now()
 difference = end_time - start_time
 print("\nTime taken:", difference)
 
-
 ###### To make new predictions ######
 
 import numpy as np
 from keras.preprocessing import image
+
 
 def new_prediction(image_path):
     test_image = image.load_img(image_path, target_size=(64, 64))
@@ -114,5 +111,6 @@ def new_prediction(image_path):
     else:
         return 'cat'
 
+
 for image in glob.glob('dataset/single_prediction/*.jpg'):
-    print("The prediction is: ",new_prediction(image))
+    print("The prediction is: ", new_prediction(image))
