5a51908c93680570ccf91ee0b8ca1265950bd770
Siddharth Krishan <sidd_krishan@outlook.com>
2019-09-11 17:44:07 -0500

    Iterative change

--- CNN.py
+++ CNN.py
@@ -30,19 +30,19 @@ classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 classifier.add(Flatten())
 
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dropout(rate=0.2))
-
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dropout(rate=0.2))
-
-classifier.add(Dense(units=64, activation='relu'))
-classifier.add(Dropout(rate=0.1))
-
 classifier.add(Dense(units=64, activation='relu'))
 classifier.add(Dropout(rate=0.2))
 
 classifier.add(Dense(units=32, activation='relu'))
+classifier.add(Dropout(rate=0.2))
+
+classifier.add(Dense(units=8, activation='relu'))
+classifier.add(Dropout(rate=0.1))
+
+classifier.add(Dense(units=4, activation='relu'))
+classifier.add(Dropout(rate=0.2))
+
+classifier.add(Dense(units=2, activation='relu'))
 #classifier.add(Dropout(rate=0.1))
 
 classifier.add(Dense(units=1, activation='sigmoid'))
